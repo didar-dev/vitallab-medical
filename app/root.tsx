8348fdd467197674ead42b5bf5f5a681aa0858b8
Didar Abdulkhaliq <me@didar.dev>
2026-01-16 10:03:22 +0300

    refactor(Layout): remove unused font links to streamline the layout component

--- app/root.tsx
+++ app/root.tsx
@@ -11,19 +11,6 @@ import type { Route } from "./+types/root";
 import { Navbar } from "./components/Navbar";
 import "./app.css";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
-
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -45,7 +32,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         />
         <meta name="apple-mobile-web-app-title" content="Vital Lab" />
         <link rel="manifest" href="/site.webmanifest" />
-
         <Meta />
         <Links />
       </head>
